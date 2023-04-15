8d28aa8f7fa98e67e15bae15975fe913a5eafb9e
Hunter2001 <eduard.boxing2001@gmail.com>
2023-04-15 13:55:29 +0300

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes`;
+export const KEY = `a12b23b2-c3e3-461d-bce2-0dce84ae8972`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const MODAL_CLOSE_SEC = 2.5;
